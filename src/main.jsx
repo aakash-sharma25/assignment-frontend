8dae14fa0b254d4bdbd15d117c9dd13a8afc6304
aakash-sharma25 <as024harma@gmail.com>
2025-01-13 18:12:37 +0530

    fixed error

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
